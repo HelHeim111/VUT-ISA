ee095538f5ad91d54d45a67d677e252f9e8c3634
HelHeim111 <dwp2004@icloud.com>
2023-11-11 19:35:51 +0100

    Almost finished

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 NAME = dhcp_stats
 TARGET = dhcp-stats
+FLAGS = -lpcap
 CC = gcc
 
 all: $(NAME).c
-	$(CC) -o $(TARGET) $(NAME).c
+	$(CC) -o $(TARGET) $(NAME).c  $(FLAGS)
 
 clean:
 	rm -f $(TARGET)
--- dhcp_stats.c
+++ dhcp_stats.c
@@ -7,26 +7,57 @@
 #include <syslog.h>
 #include <getopt.h>
 
-// Structure to hold information about the IP prefix
+int prefixCount = 0;
+
+//structure to hold information about the IP prefix
 struct PrefixInfo {
     char* prefix;
     int maxHosts;
     int allocated;
 };
 
-int isIpInPrefix(char* prefix, char* ip, char* mask) {
-    struct in_addr ipAddr, prefixAddr, maskAddr;
-    if(inet_aton(ip, &ipAddr) == 0 || inet_aton(mask, &maskAddr) == 0 || inet_aton(prefix, &prefixAddr) == 0) {
+int isIpInPrefix(char* prefix, in_addr_t ip) {
+    struct in_addr prefixAddr;
+
+    if (inet_aton(prefix, &prefixAddr) == 0) {
         return -1;
     }
-    if((ipAddr.s_addr & maskAddr.s_addr) == prefixAddr.s_addr) {
-        return 1;
-    }
-    return 0;
+
+    in_addr_t ipNetworkPart = ip & prefixAddr.s_addr;
+    return ipNetworkPart == prefixAddr.s_addr;
 }
 
-void handlePacket(const unsigned char* packet, struct PrefixInfo* prefixes, const struct udphdr* udp) {
-    //todo
+void handlePacket(const unsigned char* packet, struct PrefixInfo* prefixes) {
+
+    struct in_addr source_ip;
+    const unsigned char* dhcp_offset = packet + 26;  
+
+    memcpy(&source_ip.s_addr, dhcp_offset, 4);
+
+    for (int i = 0; i < prefixCount; i++) {
+        char* token, *netPrefix, *cidr_notation;
+        char* prefixCopy = strdup(prefixes[i].prefix);
+
+        token = strtok(prefixCopy, "/");
+        netPrefix = (char*)malloc(strlen(token) + 1);
+        strcpy(netPrefix, token);
+        token = strtok(NULL, "/");
+        cidr_notation = (char*)malloc(strlen(token) + 1);
+        strcpy(cidr_notation, token);
+        
+        if(prefixes[i].maxHosts == 0) {
+            prefixes[i].maxHosts = 1 << (32 - atoi(cidr_notation));
+        }
+        if(isIpInPrefix(netPrefix, source_ip.s_addr)) {
+            prefixes[i].allocated++;
+            double percentage = (double)(prefixes[i].allocated) * 100.00 / (double)(prefixes[i].maxHosts);
+            printf("%s %d %d %.2f%%\n", prefixes[i].prefix, prefixes[i].maxHosts, prefixes[i].allocated, percentage);
+        }
+
+        free(netPrefix);
+        free(cidr_notation);
+        free(prefixCopy);
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -34,7 +65,6 @@ int main(int argc, char* argv[]) {
     char* interface = NULL;
     char* filename = NULL;
     struct PrefixInfo *prefixes = NULL;
-    int prefixCount = 0;
     int prefixesMax = argc - 1;
     char errbuf[PCAP_ERRBUF_SIZE];
 
@@ -65,34 +95,38 @@ int main(int argc, char* argv[]) {
         prefixes[prefixCount].allocated = 0;
         prefixCount++;
     }
-
-    printf("Interface: %s\n", interface);
-    printf("Filename: %s\n", filename);
-    printf("IP Prefixes:\n");
-    for (int i = 0; i < prefixCount; i++) {
-        printf("%s\n", prefixes[i].prefix);
-    }
-
-    //opening network interface
-    pcap_t* handle = pcap_open_live(interface, BUFSIZ, 1, 1000, errbuf);
     
-    if (handle == NULL) {
-        fprintf(stderr, "Error opening device: %s\n", errbuf);
-        return 1;
+    pcap_t* handle = pcap_open_live(interface, BUFSIZ, 1, 1000, errbuf);
+
+    if(filename != NULL) {
+        //open pcap file for reading
+        handle = pcap_open_offline(filename, errbuf);
+        if (handle == NULL) {
+            fprintf(stderr, "Error opening pcap file: %s\n", errbuf);
+            return 1;
+        }
     }
 
-    //setting filter for intercepting dhcp packets
-    char* filter = "udp port 67 or udp port 68";
-    struct bpf_program bfp;
+    else if(interface != NULL) {
+        //opening network interface
+        if (handle == NULL) {
+            fprintf(stderr, "Error opening device: %s\n", errbuf);
+            return 1;
+        }
 
-    if (pcap_compile(handle, &bfp, filter, 0, PCAP_NETMASK_UNKNOWN) == -1) {
-        fprintf(stderr, "Error compiling filter: %s\n", pcap_geterr(handle));
-        return 1;
-    }
+        //setting filter for intercepting dhcp packets
+        char* filter = "udp port 67 or udp port 68";
+        struct bpf_program bfp;
 
-    if (pcap_setfilter(handle, &bfp) == -1) {
-        fprintf(stderr, "Error setting filter: %s\n", pcap_geterr(handle));
-        return 1;
+        if (pcap_compile(handle, &bfp, filter, 0, PCAP_NETMASK_UNKNOWN) == -1) {
+            fprintf(stderr, "Error compiling filter: %s\n", pcap_geterr(handle));
+            return 1;
+        }
+
+        if (pcap_setfilter(handle, &bfp) == -1) {
+            fprintf(stderr, "Error setting filter: %s\n", pcap_geterr(handle));
+            return 1;
+        }
     }
 
     while (1) {
@@ -102,10 +136,9 @@ int main(int argc, char* argv[]) {
         if (packet == NULL) {
             continue;
         }
-        //add handling here
+        handlePacket(packet, prefixes);
     }
-
-
+    
     //free alocated memory
     free(interface);
     free(filename);
